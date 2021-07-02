5d52cadd550e013a3dabddc3c895e51b22e9e9db
akkySrivastava <akkyjaicar@gmail.com>
2021-07-02 09:48:20 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -12,6 +16,7 @@ db.connect();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
+app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
@@ -29,6 +34,8 @@ app.get("*", (req, res) => {
     res.send("Welcome to mern-blog");
   }
 });
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`MERN-Blog API is running on PORT No- ${PORT}`);
